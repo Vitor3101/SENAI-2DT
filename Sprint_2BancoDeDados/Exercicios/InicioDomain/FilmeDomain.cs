794ded506986ff06f3f03ff5f377dda1b326257f
FIC <FIC@info.local>
2021-08-24 16:48:48 -0300

    24.08

---
+++ Sprint_2BancoDeDados/Exercicios/InicioDomain/FilmeDomain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoFilmesAspWeb.Domain
+{
+    public class FilmeDomain
+    {
+        public int idFilme { get; set; }
+        public int idGernero { get; set; }
+        public string TituloFilme { get; set; }
+    }
+}
